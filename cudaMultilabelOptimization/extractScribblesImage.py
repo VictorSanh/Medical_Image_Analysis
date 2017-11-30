Correct computation of the groundTruth from matlab files and more robust for name files with several points

cudaMultilabelOptimization/extractScribblesImage.py
@@ -81,7 +81,7 @@ class Image:
         self.set_scribble1 = None
         self.rand_scribble = None
 
-        if name_file.split('.')[1] == 'txt':
+        if name_file.split('.')[-1] == 'txt':
             """ Extract a matrix containing the data of the text file
             Each line of the matrix corresponds to a line on the file
             Each element of a line are separated by "delimiter"
@@ -105,9 +105,8 @@ class Image:
                 for j in range(len(data[0])):
                     self.labels[i][j] = data[i][j]
 
-        elif name_file.split('.')[1] == 'mat':
+        elif name_file.split('.')[-1] == 'mat':
             self.labels = load_labels_from_Berkeley_dataset(name_file, segmentation_num)
-
 
         else:
             print("File is neither a texte file nor a matlab file : the program do not know how to manage it")
@@ -118,19 +117,20 @@ class Image:
         elif (np.unique(self.labels) == [0, 1]).all() and self.label0 == 1:
             self.labels += 1
 
-        if name_file.split('.')[1] == 'mat':
+        if name_file.split('.')[-1] == 'mat':
             str_data = ''
-            for i in range(self.labels.shape[1]):
-                for j in range(self.labels.shape[0]):
-                    str_data += str(int(self.labels[j,i])) + delimiter
+            for i in range(self.labels.shape[0]):
+                for j in range(self.labels.shape[1]):
+                    str_data += str(int(self.labels[i,j])) + delimiter
                 str_data = str_data[:-len(delimiter)] + '\n'
 
-            with open(name_file.split('.')[0] + ".txt", "w") as text_file:
+            with open(name_file[:-4] + ".txt", "w") as text_file:
                 text_file.write(str_data)
 
             print("Txt file From Mat saved\n")
 
         self.size_y, self.size_x = np.shape(self.labels)
+    
 
     def show_labels(self):
         plt.matshow(self.labels, origin='upper')